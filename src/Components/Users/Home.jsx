5bae51e3bb6fff7be732d29bcfa7707b27a7e043
abdulhakeem1232 <hakeemforgit@gmail.com>
2024-10-25 19:40:16 +0530

    socket testing

--- src/Components/Users/Home.jsx
+++ src/Components/Users/Home.jsx
@@ -8,7 +8,8 @@ import io from 'socket.io-client';
 
 // const socket = io('http://localhost:3001');
 const socket = io('https://kiara-server.vercel.app', {
-  transports: ['websocket']
+  transports: ['websocket'],
+  reconnectionAttempts: 5 
 });
 
 
